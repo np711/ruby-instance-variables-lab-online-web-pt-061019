5f23eaa3fc138c75ed0585bf67b37accbdfc4efd
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-22 14:27:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,10 @@ class Dog
     
     this_dogs_name =
     dog_name
-    "@this_dogs_name"
-    'Lassie'
-    
   end
+  lassie = Dog.new 
+  lassie.name = "Lassie"
+  lassie.name 
   
   def name
     
